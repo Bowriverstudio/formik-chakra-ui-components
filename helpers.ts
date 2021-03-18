e94f1626628b2992ad8a9fefa1967fc0278dc547
Maurice Tadros <maurice@bowriverstudio.com>
2021-03-18 11:55:56 -0600

    created helper function createInitalValues

--- helpers.ts
+++ helpers.ts
@@ -1,33 +1,28 @@
-import * as yup from "yup";
-
-// export function createYupSchema(schema, config) {
-//     const { id, validationType, validations = [] } = config;
-//     if (!yup[validationType]) {
-//         return schema;
-//     }
-//     let validator = yup[validationType]();
-//     validations.forEach(validation => {
-//         const { params, type } = validation;
-//         if (!validator[type]) {
-//             return;
-//         }
-//         validator = validator[type](...params);
-//     });
-//     schema[id] = validator;
-//     return schema;
-// }  
-
-import { FormikSchema, FormikSchemaField } from "../../../types";
+mport { FormikSchema, FormikSchemaField } from "../../../types";
 import * as Yup from "yup";
+/**
+ * Creates the Initial values from the configuration.
+ * 
+ * Usage:
+ * 
+ * @param schema 
+ * @returns { {['name']: value}}
+ */
+export function createInitialValues(schema: FormikSchema) {
+    const initialValues = {};
+    schema.forEach(item => {
+        initialValues[item.name] = item.value || "";
+    });
 
-
+    return initialValues;
+}
 
 export function createYupSchema(schema: FormikSchema) {
     const yepSchema = schema.reduce(_createYupSchema, {});
     return Yup.object().shape(yepSchema);
 }
 // Code taken from: https://codesandbox.io/s/clever-snyder-1u410?fontsize=14&file=/src/yupSchemaCreator.js:0-485
-export function _createYupSchema(schema, config: FormikSchemaField) {
+function _createYupSchema(schema, config: FormikSchemaField) {
 
     const { name, validationType, validations = [] } = config;
 
@@ -50,4 +45,4 @@ export function _createYupSchema(schema, config: FormikSchemaField) {
     });
     schema[name] = validator;
     return schema;
-}  
+}
